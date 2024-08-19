18fa6baac96fa7386a4171a2453c0f7eab635521
Celitech <80042395+zgheibali@users.noreply.github.com>
2024-08-19 10:57:13 +0000

    liblab SDK update for version v1.1.86

--- src/services/BaseService.php
+++ src/services/BaseService.php
@@ -4,6 +4,10 @@ namespace Celitech\Services;
 
 use Celitech\Environment;
 use Celitech\Retry;
+use Celitech\Hooks\CustomHook;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 
@@ -28,6 +32,9 @@ class BaseService
 
         $stack->push(Retry::factory());
 
+        $hook = new CustomHook(['clientId' => $clientId, 'clientSecret' => $clientSecret]);
+        $stack->push($this->hookMiddleware($hook));
+
         $this->client = new Client([
             'handler' => $stack,
         ]);
@@ -46,5 +53,26 @@ class BaseService
     public function setBaseUrl(string $url): void
     {
         $this->baseUrl = $url;
+    }
+
+    private function hookMiddleware(CustomHook $hook)
+    {
+        return function (callable $handler) use ($hook) {
+            return function (RequestInterface $request, array $options) use ($handler, $hook) {
+                $hook->beforeRequest($request);
+
+                return $handler($request, $options)->then(
+                    function (ResponseInterface $response) use ($request, $hook) {
+                        $hook->afterResponse($request, $response);
+                        return $response;
+                    },
+                    function ($reason) use ($request, $hook) {
+                        if ($reason instanceof Exception) {
+                            $hook->onError($request, $reason);
+                        }
+                    }
+                );
+            };
+        };
     }
 }
