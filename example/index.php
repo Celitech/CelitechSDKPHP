a854c1f14df43045112a4a7ded8f5c4a1136fe4a
Celitech <80042395+zgheibali@users.noreply.github.com>
2024-11-07 12:56:37 +0000

    liblab SDK update for version v1.2.9

--- example/index.php
+++ example/index.php
@@ -7,9 +7,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Celitech\Models;
 
 use Celitech\Client;
+use Celitech\Models\GetAccessTokenRequest;
 
 $sdk = new Client(clientId: 'client-id', clientSecret: 'client-secret');
 
-$response = $sdk->Destinations->listDestinations();
+$input = new Models\GetAccessTokenRequest(grantType: $grantType, clientId: 'client_id', clientSecret: 'client_secret');
+
+$response = $sdk->OAuth->getAccessToken(input: $input);
 
 print_r($response);
