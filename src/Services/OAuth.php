ec5fb61e762e0e47bbb15ed535ac232ffa94e002
Celitech <80042395+zgheibali@users.noreply.github.com>
2024-11-08 10:29:45 +0000

    liblab SDK update for version v1.3.2

---
+++ src/Services/OAuth.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Celitech\Services;
+
+use Celitech\Utils\Serializer;
+use Celitech\Models;
+
+class OAuth extends BaseService
+{
+    /**
+     * This endpoint was added by liblab
+     */
+    public function getAccessToken(Models\GetAccessTokenRequest $input): Models\GetAccessTokenOkResponse
+    {
+        $data = $this->sendRequest('post', '/oauth2/token', ['form_params' => $input]);
+
+        return Serializer::deserialize($data, Models\GetAccessTokenOkResponse::class);
+    }
+}
