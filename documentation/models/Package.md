0a9d5e9e01c274a5a657c877e0f83b37cef42ca6
zgheibali <80042395+zgheibali@users.noreply.github.com>
2025-06-21 13:44:19 +0300

    liblab SDK update for version v1.3.44 (#112)

--- documentation/models/Package.md
+++ documentation/models/Package.md
@@ -8,7 +8,7 @@
 | :-------- | :----------| :----------| :----------|
     | id | string | ❌ | ID of the package |
     | dataLimitInBytes | number | ❌ | Size of the package in Bytes |
-    | destination | string | ❌ | ISO representation of the package's destination |
+    | destination | string | ❌ | ISO representation of the package's destination. |
     | destinationName | string | ❌ | Name of the package's destination |
     | priceInCents | number | ❌ | Price of the package in cents |
 
--- src/Models/TopUpEsimRequest.php
+++ src/Models/TopUpEsimRequest.php
@@ -13,7 +13,7 @@ class TopUpEsimRequest
     public string $iccid;
 
     /**
-     * Size of the package in GB. The available options are 1, 2, 3, 5, 8, 20GB
+     * Size of the package in GB. The available options are 0.5, 1, 2, 3, 5, 8, 20GB
      */
     #[SerializedName('dataLimitInGB')]
     public float $dataLimitInGb;
@@ -31,7 +31,7 @@ class TopUpEsimRequest
     public string $endDate;
 
     /**
-     * Email address where the purchase confirmation email will be sent (excluding QR Code & activation steps)
+     * Email address where the purchase confirmation email will be sent (excluding QR Code & activation steps).
      */
     #[SerializedName('email')]
     public ?string $email;
@@ -41,6 +41,12 @@ class TopUpEsimRequest
      */
     #[SerializedName('referenceId')]
     public ?string $referenceId;
+
+    /**
+     * Customize the email subject brand. The `emailBrand` parameter cannot exceed 25 characters in length and must contain only letters, numbers, and spaces. This feature is available to platforms with Diamond tier only.
+     */
+    #[SerializedName('emailBrand')]
+    public ?string $emailBrand;
 
     /**
      * Epoch value representing the start time of the package's validity. This timestamp can be set to the current time or any time within the next 12 months.
@@ -61,6 +67,7 @@ class TopUpEsimRequest
         string $endDate,
         ?string $email = null,
         ?string $referenceId = null,
+        ?string $emailBrand = null,
         ?float $startTime = null,
         ?float $endTime = null
     ) {
@@ -70,6 +77,7 @@ class TopUpEsimRequest
         $this->endDate = $endDate;
         $this->email = $email;
         $this->referenceId = $referenceId;
+        $this->emailBrand = $emailBrand;
         $this->startTime = $startTime;
         $this->endTime = $endTime;
     }
