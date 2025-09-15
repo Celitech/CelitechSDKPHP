e97331ca612864f32d3fce60784ea8030970e030
Celitech <80042395+zgheibali@users.noreply.github.com>
2025-09-15 14:28:02 +0000

    liblab SDK update for version v1.3.53

--- src/OAuth/TokenManager.php
+++ src/OAuth/TokenManager.php
@@ -43,18 +43,19 @@ class TokenManager
 
     public function getToken(array $scopes): OauthToken
     {
-        if ($this->token && !array_diff($scopes, $this->token->scopes)) {
+        $timestamp = (new DateTime('now', new DateTimeZone('UTC')))->getTimestamp();
+        $has_all_scopes = $this->token && !array_diff($scopes, $this->token->scopes);
+        $valid_token =
+            $this->token && ($this->token->expiresAt === null || $this->token->expiresAt - $timestamp > 5000);
+        if ($has_all_scopes && $valid_token) {
             return $this->token;
         }
 
         if ($this->token) {
             $scopes = array_unique(array_merge($scopes, $this->token->scopes));
         }
-
         $response = $this->getAccessToken($scopes);
-        $expiresAt = isset($response['expires_in'])
-            ? (new DateTime('now', new DateTimeZone('UTC')))->getTimestamp() + $response['expires_in']
-            : null;
+        $expiresAt = isset($response['expires_in']) ? $timestamp + $response['expires_in'] : null;
 
         $this->token = new OauthToken($response['access_token'], $scopes, $expiresAt);
         return $this->token;
