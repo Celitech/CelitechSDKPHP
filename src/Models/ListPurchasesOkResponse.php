d00d322b43adafa78c001a32ccf8e0604b0f8e2e
Celitech <80042395+zgheibali@users.noreply.github.com>
2025-10-06 15:48:16 +0000

    liblab SDK update for version v1.3.57

--- src/Models/ListPurchasesOkResponse.php
+++ src/Models/ListPurchasesOkResponse.php
@@ -7,18 +7,18 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
 class ListPurchasesOkResponse
 {
     /**
-     * @var Purchases[]|null
+     * @var Purchases[]
      */
     #[SerializedName('purchases')]
-    public ?array $purchases;
+    public array $purchases;
 
     /**
      * The cursor value representing the end of the current page of results. Use this cursor value as the "afterCursor" parameter in your next request to retrieve the subsequent page of results. It ensures that you continue fetching data from where you left off, facilitating smooth pagination.
      */
     #[SerializedName('afterCursor')]
-    public ?string $afterCursor;
+    public string $afterCursor;
 
-    public function __construct(?array $purchases = [], ?string $afterCursor = null)
+    public function __construct(array $purchases, string $afterCursor)
     {
         $this->purchases = $purchases;
         $this->afterCursor = $afterCursor;
