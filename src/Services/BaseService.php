3bb9eb402b377fd23643394dd3110525b97eb7a0
elmasrisaer <139119606+elmasrisaer@users.noreply.github.com>
2026-07-20 12:26:10 +0300

    SDK update for version v2.0.6 (#169)

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -41,7 +41,7 @@ class BaseService
   ) {
     $this->options = [
       'headers' => [
-        'User-Agent' => 'postman-codegen/1.6.0 celitech-sdk/sdk/2.0.5 (php)'
+        'User-Agent' => 'postman-codegen/1.7.0 celitech-sdk/sdk/2.0.6 (php)'
       ]
     ];
 
